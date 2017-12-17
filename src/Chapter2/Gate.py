6b32a28a114064157d76e4c320f2f3abdcb63920
NaomichiFurushima <>
2017-12-17 15:50:17 +0900

    ORの処理をGATEで置き換え

--- src/Chapter2/Gate.py
+++ src/Chapter2/Gate.py
@@ -10,12 +10,4 @@ def AND(signal):
     return GATE(map(lambda x: (x, 0.5), signal), -0.7, 0)
 
 def OR(signal):
-    w = 0.5
-    b = -0.2
-    theta = 0 
-    d = map(lambda x: (x, w), signal)
-    t = b + reduce(add, map(lambda x: x[0] * x[1], d))
-    if t <= theta:
-        return 0
-    else:
-        return 1
+    return GATE(map(lambda x: (x, 0.5), signal), -0.2, 0)
