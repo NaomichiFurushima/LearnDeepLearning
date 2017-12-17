e5ddfcda2567a9978df608ea2b13fdafb509f822
NaomichiFurushima <>
2017-12-17 15:41:36 +0900

    AND実装　テストが通る状態

--- src/Chapter2/Gate.py
+++ src/Chapter2/Gate.py
@@ -1,3 +1,11 @@
-
+from operator import add
 def AND(signal):
-    return 0
+    w = 0.5
+    b = -0.7
+    theta = 0 
+    d = map(lambda x: (x, w), signal)
+    t = b + reduce(add, map(lambda x: x[0] * x[1], d))
+    if t <= theta:
+        return 0
+    else:
+        return 1
