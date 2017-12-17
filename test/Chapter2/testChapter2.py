785dd074a748ce05f9402752214568bd4f6ceae0
NaomichiFurushima <>
2017-12-17 14:32:01 +0900

    ANDゲートのテスト

---
+++ test/Chapter2/testChapter2.py
@@ -0,0 +1,11 @@
+import sys, os
+sys.path.append("../../src/Chapter2")
+import Gate as g
+import unittest
+
+class Chapter2Test(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(0, g.gate([{0:0.5},{1:0.5}],-0.7))
+
+if __name__ == "__main__":
+    unittest.main()
