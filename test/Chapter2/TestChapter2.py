cee6c8a2c845bbc056d0b3f9ca1baaef0a9b2618
NaomichiFurushima <>
2017-12-17 15:45:24 +0900

    入力信号が書き換えられるとよろしく無さそうなのでタプルに

--- test/Chapter2/TestChapter2.py
+++ test/Chapter2/TestChapter2.py
@@ -5,15 +5,15 @@ import unittest
 
 class TestTestTest(unittest.TestCase):
     def test_and(self):
-        self.assertEqual(0, g.AND([0, 0]))
-        self.assertEqual(0, g.AND([0, 1]))
-        self.assertEqual(1, g.AND([1, 1]))
-        self.assertEqual(0, g.AND([1, 0]))
+        self.assertEqual(0, g.AND((0, 0)))
+        self.assertEqual(0, g.AND((0, 1)))
+        self.assertEqual(1, g.AND((1, 1)))
+        self.assertEqual(0, g.AND((1, 0)))
     def test_or(self):
-        self.assertEqual(0, g.OR([0, 0]))
-        self.assertEqual(1, g.OR([0, 1]))
-        self.assertEqual(1, g.OR([1, 1]))
-        self.assertEqual(1, g.OR([1, 0]))
+        self.assertEqual(0, g.OR((0, 0)))
+        self.assertEqual(1, g.OR((0, 1)))
+        self.assertEqual(1, g.OR((1, 1)))
+        self.assertEqual(1, g.OR((1, 0)))
 
 if __name__ == "__main__":
     unittest.main()
