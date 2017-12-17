022f5faba8fd30a2afd91bc632469569a9879202
NaomichiFurushima <>
2017-12-17 14:56:58 +0900

    ANDゲートとりあえず実装 テストは通る

--- src/Chapter2/Gate.py
+++ src/Chapter2/Gate.py
@@ -1,3 +1,7 @@
-
-def AND (x, b):
-    return 0
+from operator import add
+def AND (signal, bias, theta = 0):
+    t = bias + reduce(add, map(lambda xw : xw[0] * xw[1], signal))
+    if t <= 0:
+        return 0
+    else:
+        return 1
--- test/Chapter2/testChapter2.py
+++ test/Chapter2/testChapter2.py
@@ -5,10 +5,10 @@ import unittest
 
 class Chapter2Test(unittest.TestCase):
     def test_and(self):
-        self.assertEqual(0, g.AND([{0:0.5},{0:0.5}],-0.7))
-        self.assertEqual(0, g.AND([{0:0.5},{1:0.5}],-0.7))
-        self.assertEqual(1, g.AND([{1:0.5},{1:0.5}],-0.7))
-        self.assertEqual(0, g.AND([{0:0.5},{0:0.5}],-0.7))
+        self.assertEqual(0, g.AND([(0,0.5),(0,0.5)],-0.7))
+        self.assertEqual(0, g.AND([(0,0.5),(1,0.5)],-0.7))
+        self.assertEqual(1, g.AND([(1,0.5),(1,0.5)],-0.7))
+        self.assertEqual(0, g.AND([(0,0.5),(0,0.5)],-0.7))
 
 if __name__ == "__main__":
     unittest.main()
