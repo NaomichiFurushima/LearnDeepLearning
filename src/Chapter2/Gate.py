77e03358b5980b6c45896e058d14c9bc42261803
NaomichiFurushima <>
2017-12-17 15:49:23 +0900

    共通処理をまとめる　ANDの処理の一部を置き換え

--- src/Chapter2/Gate.py
+++ src/Chapter2/Gate.py
@@ -1,14 +1,13 @@
 from operator import add
-def AND(signal):
-    w = 0.5
-    b = -0.7
-    theta = 0 
-    d = map(lambda x: (x, w), signal)
-    t = b + reduce(add, map(lambda x: x[0] * x[1], d))
+def GATE(sw, bias, theta):
+    t = bias + reduce(add, map(lambda x: x[0] * x[1], sw))
     if t <= theta:
         return 0
     else:
         return 1
+
+def AND(signal):
+    return GATE(map(lambda x: (x, 0.5), signal), -0.7, 0)
 
 def OR(signal):
     w = 0.5
