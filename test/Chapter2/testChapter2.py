dcdb10cf94145c5b8cf64903c7a83661da0574c3
NaomichiFurushima <>
2017-12-17 14:34:51 +0900

    ANDのテストパターン全て追加(失敗する)

--- test/Chapter2/testChapter2.py
+++ test/Chapter2/testChapter2.py
@@ -4,8 +4,11 @@ import Gate as g
 import unittest
 
 class Chapter2Test(unittest.TestCase):
-    def test_add(self):
-        self.assertEqual(0, g.gate([{0:0.5},{1:0.5}],-0.7))
+    def test_and(self):
+        self.assertEqual(0, g.AND([{0:0.5},{0:0.5}],-0.7))
+        self.assertEqual(0, g.AND([{0:0.5},{1:0.5}],-0.7))
+        self.assertEqual(1, g.AND([{1:0.5},{1:0.5}],-0.7))
+        self.assertEqual(0, g.AND([{0:0.5},{0:0.5}],-0.7))
 
 if __name__ == "__main__":
     unittest.main()
